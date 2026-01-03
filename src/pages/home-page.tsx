11801aba7da6c9237de5c1335de356a54b867b36
erinskieasy <xx876xx@gmail.com>
2026-01-02 21:08:02 -0500

    feat: Implement initial client-server setup with Express, Vite, and Tailwind CSS.

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,9 +1,12 @@
 export default function HomePage() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-black">
-            <h1 className="text-9xl font-bold text-white animate-float">
+        <div className="min-h-screen flex flex-col items-center justify-center bg-black relative gap-4">
+            <h1 className="text-9xl font-bold text-white animate-float leading-none">
                 Hello World
             </h1>
+            <p className="text-white text-center">
+                Blank Template by <i>Erinski Easy</i>
+            </p>
         </div>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
+    server: {
+      port: parseInt(env.CLIENT_PORT) || 5173,
+      proxy: {
+        '/api': 'http://localhost:5000',
+      },
     },
-  },
+  }
 })
